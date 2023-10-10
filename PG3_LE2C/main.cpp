a641adc3d3c8d493bddb43e9156abd549677a723
ogura715924 <115062896+ogura715924@users.noreply.github.com>
2023-10-10 10:10:44 +0900

    途中

---
+++ PG3_LE2C/main.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+template<typename Type>
+
+Type Min(Type a, Type b) {
+	int c = 0;
+	if (a < b) {
+		c = a;
+	}
+	else if (a > b) {
+		c = b;
+	}
+
+	return static_cast<Type>(c);
+}
+
+template<>
+char Min<char>(char a, char b) {
+	a = printf("”šˆÈŠO‚Í“ü—Í‚Å‚«‚Ü‚¹‚ñ");
+	return a;
+}
+
+
+int main(void) {
+	printf("%d\n", Min<int>(114, 551));
+	printf("%f\n", Min< float>(114.0f, 551.4f));
+	printf("%lf\n", Min< double>(114.0f, 551.4f));
+	printf("%c\n", Min<char>('a', 'b'));
+
+	return 0;
+}
