4c006cce4708297126059d42bc24b3d956287c2f
ogura715924 <115062896+ogura715924@users.noreply.github.com>
2023-10-18 18:23:50 +0900

    できた

--- PG3_LE2C/main.cpp
+++ PG3_LE2C/main.cpp
@@ -1,35 +1,29 @@
 #include<stdio.h>
 
-int m = 100;
-
-int Recursive(int n) {
+int Tortal(int n) {
 	if (n >= 1) {
-		return 1072*n;
+		return 1072 * n;
 	}
-	return(n*Recursive(n-1));
+	return(n * Tortal(n - 1));
 }
 
+int Recursive(int time, int many) {
+	if (time <= 1) {
 
-
-int Tortal(int t ) {
-	if (t == 1) {
-
-
-		return m;
+		return many;
 	}
-	m = m * 2 - 50;
-	return (Tortal(t - 1));
-	
+	//a = a * 2 - 50;
+	return (many + Recursive(time - 1, many*2-50));
+
 }
-
-
 
 int main() {
-	int s = 9;//“­‚¢‚½ŽžŠÔ
-	int n = Recursive(s);
-	int t = Tortal(s);
-	printf("’À‹à‘ÌŒn:%d\n",n);
-	printf("Ä‹A“I‚È’À‹à:%d",t);
+	int work = 8;//“­‚¢‚½ŽžŠÔ
+	int tortal = Recursive(work, 100);
+	int recursive = Tortal(work);
+	printf("%dŽžŠÔ“­‚¢‚½Žž‚Ì’À‹à‘ÌŒn:%d\n", work, recursive);
+	printf("%dŽžŠÔ“­‚¢‚½Žž‚ÌÄ‹A“I‚È’À‹à:%d\n", work, tortal);
+	
 
 	return(0);
 }
