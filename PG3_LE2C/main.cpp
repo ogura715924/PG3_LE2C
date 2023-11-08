e6cb338415e6cbdb0dbde759b21feb281b7890b6
ogura715924 <115062896+ogura715924@users.noreply.github.com>
2023-11-08 17:45:20 +0900

    3うつし

---
+++ PG3_LE2C/main.cpp
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<Windows.h>
+#include <stdlib.h>
+#include<time.h>
+
+
+
+typedef void (*PFunc)(int*);
+int memory;
+int dice;
+
+
+
+//コールバック関数
+void DispResult(int* s) {
+	printf("%d秒待った\n", *s);
+
+	printf("答え：%d\n", dice);
+
+}
+
+
+
+void setTimeout(PFunc p, int second) {
+	//コールバック関数を呼び出す,3秒待つ
+	Sleep(second * 1000);
+
+	// 結果表示
+	p(&second);
+}
+
+
+int main() {
+
+	printf("奇数なら1を偶数なら0を押してください\n");
+
+	// プレイヤーの入力処理、奇数か偶数を入力させる
+	int mj;
+
+	// サイコロ 1 ~ をふる
+	unsigned int currentTime = time(nullptr);
+	srand(currentTime);
+	dice = rand() % 6 + 1;
+	memory = dice;
+
+	scanf_s("%d", &mj);
+	if (mj == 1) {
+		printf("奇数\n");
+	}
+	else if (mj == 0) {
+		printf("偶数\n");
+	}
+
+	PFunc p;
+
+	p = DispResult;
+	setTimeout(p, 3);
+
+
+
+	memory %= 2;
+	if (mj == memory) {
+		printf("正解\n");
+	}
+	else {
+		printf("不正解\n");
+	}
+
+	return 0;
+}
