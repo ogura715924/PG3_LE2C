8467cccd4fc9b0672c59ec1a2921cba64d544477
ogura715924 <115062896+ogura715924@users.noreply.github.com>
2023-11-08 17:40:49 +0900

    確認課題01_03

--- PG3_LE2C/main.cpp
+++ PG3_LE2C/main.cpp
@@ -1,51 +1,70 @@
 #include<stdio.h>
 #include<Windows.h>
-#/*include <stdlib.h>
+#include <stdlib.h>
 #include<time.h>
-int count=3;
-
-int Sai(int a) {
-	
-	return 0;
-}
-
-
-
-int main(void) {
-	
-	printf("奇数なら1を偶数なら2を押してください\n");
-
-	
-
-	count--;
-
-	unsigned int currentTime = time(nullptr);
-	srand(currentTime);
-	int num = 1+rand()%10;
-	if (count <= 0) {
-		printf("%d\n", num);
-	}
-
-
-
-	return 0;
-}*/
 
 
 
 typedef void (*PFunc)(int*);
+int memory;
+int dice;
 
+
+
+//コールバック関数
 void DispResult(int* s) {
 	printf("%d秒待った\n", *s);
+
+	printf("答え：%d\n", dice);
+
 }
 
-void serTimeout(PFunc p, int second) {
+
+
+void setTimeout(PFunc p, int second) {
+	//コールバック関数を呼び出す,3秒待つ
 	Sleep(second * 1000);
+
+	// 結果表示
 	p(&second);
 }
 
+
 int main() {
-	printf("start\n");
+
+	printf("奇数なら1を偶数なら0を押してください\n");
+	
+	// プレイヤーの入力処理、奇数か偶数を入力させる
+	int mj;
+	
+	// サイコロ 1 ~ をふる
+	unsigned int currentTime = time(nullptr);
+	srand(currentTime);
+	dice = rand() % 6 + 1;
+	memory = dice;
+
+	scanf_s("%d", &mj);
+	if (mj == 1) {
+		printf("奇数\n");
+	}
+	else if (mj == 0) {
+		printf("偶数\n");
+	}
+	
 	PFunc p;
-	p=Dis
+
+	p = DispResult;
+	setTimeout(p, 3);
+
+	
+
+	memory %= 2;
+	if (mj == memory) {
+		printf("正解\n");
+	}
+	else {
+		printf("不正解\n");
+	}
+
+	return 0;
 }
