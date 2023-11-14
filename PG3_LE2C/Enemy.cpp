43c30cbfee7fbd04af294ef394437ddafd822b13
ogura715924 <115062896+ogura715924@users.noreply.github.com>
2023-11-14 11:17:49 +0900

    a

--- PG3_LE2C/Enemy.cpp
+++ PG3_LE2C/Enemy.cpp
@@ -1,1 +1,17 @@
 #include "Enemy.h"
+
+void(Enemy::* Enemy::pFuncTable[])() = {
+	&Enemy::TestFunc,
+	&Enemy::TestFunc2
+};
+
+void Enemy::Test() {
+	(this->*pFuncTable[index_])();
+}
+void Enemy::TestFunc() {
+	printf("1");
+	index_++;
+}
+void Enemy::TestFunc2() {
+	printf("2");
+}
