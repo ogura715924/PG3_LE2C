b23eb4998a51f978ef8bc296eaf23a966f596a59
ogura715924 <115062896+ogura715924@users.noreply.github.com>
2023-11-14 11:16:30 +0900

    a

--- PG3_LE2C/Enemy.h
+++ PG3_LE2C/Enemy.h
@@ -1,5 +1,16 @@
 #pragma once
-class Enemy
-{
-};
+#include <stdio.h>
 
+//自作クラス
+class Enemy {
+public:
+	//自作メンバ関数
+	void Test();
+	void TestFunc();
+	void TestFunc2();
+private:
+	//メンバ関数ポインタ
+	static void (Enemy::* pFuncTable[])();
+
+	int index_ = 0;
+};
